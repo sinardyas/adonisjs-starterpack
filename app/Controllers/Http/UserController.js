4349bbd0414157d7c808f5214c2562b6c3925764
Sinar Dyas <sinar.dp@meteor.id>
2018-12-22 17:13:32 +0700

    add pagination

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,16 +1,29 @@
 'use strict'
 
 const User = use('App/Models/User')
+const Database = use('Database')
 
 class UserController {
-    async getUser ({ view }) {
-        const users = await User.all()
+    async getUser ({ request, view }) {
+        const { page } = request.get()
+        const users = await Database.from('users').paginate(page, 10)        
 
-        return view.render('content.user', { users: users.toJSON() })
+        return view.render('content.user', { users: users })
     }
 
-    async createUser ({ view }) {
+    async createUser ({ request, response }) {
+        const { username, email, password } = request.body;
+        const user = new User()
 
+        user.username = username
+        user.email = email
+        user.password = password
+        await user.save()
+
+        return response.status(200).json({
+            status: 200,
+            success: true
+        })
     }
 }
 
